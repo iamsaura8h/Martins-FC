18b47103fe8032f86026a9df9ed58c934d1237c7
iamsaura8h <saurabhchaudhary.net@gmail.com>
2024-12-11 11:32:48 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+<div className='text-xl flex justify-center items-center font-sans font-bold bg-black w-screen h-screen'>
+  
+        <h1 className='text-red-600 text-7xl'>Welcome to Martins FC</h1>
+</div>
+    </>
+  )
+}
+
+export default App
